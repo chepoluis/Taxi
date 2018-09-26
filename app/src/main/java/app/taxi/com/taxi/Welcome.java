90635f3ef63a175ab803178805a17484016c8509
Luis <vsjoseluisvs@gmail.com>
2018-09-25 22:28:47 -0500

    Integrated presence system

--- app/src/main/java/app/taxi/com/taxi/Welcome.java
+++ app/src/main/java/app/taxi/com/taxi/Welcome.java
@@ -53,9 +53,11 @@ import com.google.android.gms.maps.model.Polyline;
 import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.gms.maps.model.SquareCap;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.iid.FirebaseInstanceId;
 
 import org.json.JSONArray;
@@ -114,6 +116,9 @@ public class Welcome extends FragmentActivity implements OnMapReadyCallback,
     private Polyline blackPolyline, greyPolyline;
 
     private IGoogleAPI mService;
+
+    // Presense system
+    DatabaseReference onlineRef, currentUserRef;
 
     Runnable drawPathRunnable = new Runnable() {
         @Override
@@ -177,17 +182,38 @@ public class Welcome extends FragmentActivity implements OnMapReadyCallback,
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        // Presense system
+        onlineRef = FirebaseDatabase.getInstance().getReference().child(".info/connected");
+        currentUserRef = FirebaseDatabase.getInstance().getReference(Common.driver_tbl)
+                .child(FirebaseAuth.getInstance().getCurrentUser().getUid());
+        onlineRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // Remove value from driver_tb1 when driver disconnected
+                currentUserRef.onDisconnect().removeValue();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
         // Init view
         location_switch = findViewById(R.id.location_switch);
         location_switch.setOnCheckedChangeListener(new MaterialAnimatedSwitch.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(boolean isOnline) {
                 if(isOnline) {
+                    FirebaseDatabase.getInstance().goOnline(); // Connected when switch turn on
+
                     startLocationUpdates();
                     displayLocation();
                     Snackbar.make(mapFragment.getView(), "You are online", Snackbar.LENGTH_LONG)
                                 .show();
                 } else {
+                    FirebaseDatabase.getInstance().goOffline(); // Disconnected when switch turn off
+
                     stopLocationUpdates();
                     mCurrent.remove();
                     mMap.clear();
